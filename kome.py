f09ff0e2b780a6f41791db1bf09ddaa4f17e929f
wan <wanhuz96@gmail.com>
2023-10-09 11:59:45 +0800

    add proper argument

--- kome.py
+++ kome.py
@@ -1,23 +1,30 @@
-import sys
+import argparse
 from module.controller import Controller
 
-def main(*args):
-
-    if (len(sys.argv) < 1):
-        print('Invalid commands! Run with --help to view all commands')
-        return
+def create_parser():
+    parser = argparse.ArgumentParser(description='Convert Japanese subtitle into English Video')
+    parser.add_argument('src', metavar='src', type=str, help='Input source video')
+    parser.add_argument('dest', metavar='dest', type=str, help='Input destination video')
     
-    args = sys.argv[1:]
+    parser.add_argument('-script', metavar='script', type=str, help='Input subtitle script to use instead of source track')
+    parser.add_argument('-track', metavar='track', type=int, help='Input subtitle track to use for source video')
 
-    if (len(args) == 3):
-        src_video = args[0]
-        dest_video = args[1]
-        sub_script = args[2]
-        controller = Controller()
+    return parser
 
-        controller.start(src_video, dest_video, sub_script)
-    else:
-        print('Invalid commands! Run with --help to view all commands')
+def main(*args):
+    
+    parser = create_parser()
+    args = parser.parse_args()
+
+    src_video = args.src
+    dest_video = args.dest
+
+    sub_script = args.script
+    track_no = args.track
+
+    controller = Controller()
+    controller.start(src_video, dest_video, 
+                     sub_script, track_no)
 
 if __name__ == "__main__":
     main()
